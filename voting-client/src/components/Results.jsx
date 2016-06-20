7963129c2f2dd90b331155f01485f93196c5c091
“idoco” <ido.c.mail@gmail.com>
2016-06-20 14:54:21 +0300

    Getting Data In from Redux to React

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 
 
-export default React.createClass({
+export const Results = React.createClass({
     mixins: [PureRenderMixin],
     getPair: function() {
         return this.props.pair || [];
@@ -18,14 +19,16 @@ export default React.createClass({
         return this.props.winner ?
             <Winner ref="winner" winner={this.props.winner} /> :
             <div className="results">
-                {this.getPair().map(entry =>
-                <div key={entry} className="entry">
-                    <h1>{entry}</h1>
-                    <div className="voteCount">
-                        {this.getVotes(entry)}
-                    </div>
-                </div>
+                <div className="tally">
+                    {this.getPair().map(entry =>
+                        <div key={entry} className="entry">
+                            <h1>{entry}</h1>
+                            <div className="voteCount">
+                                {this.getVotes(entry)}
+                            </div>
+                        </div>
                     )}
+                </div>
                 <div className="management">
                     <button ref="next"
                             className="next"
@@ -36,3 +39,13 @@ export default React.createClass({
             </div>;
     }
 });
+
+function mapStateToProps(state) {
+    return {
+        pair: state.getIn(['vote', 'pair']),
+        tally: state.getIn(['vote', 'tally']),
+        winner: state.get('winner')
+    }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
